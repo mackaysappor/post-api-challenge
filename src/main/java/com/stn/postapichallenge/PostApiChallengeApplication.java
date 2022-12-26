ccf1e9d18cdc0d569ee4c0bccd1b3228467d6e2b
mackaysappor <labs.mackay@gmail.com>
2022-12-26 12:42:05 +0000

    Initial commit

---
+++ src/main/java/com/stn/postapichallenge/PostApiChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.stn.postapichallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostApiChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostApiChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stn/postapichallenge/PostApiChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stn.postapichallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostApiChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
