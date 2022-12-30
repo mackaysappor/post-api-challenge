e17beb35fc2e232e000d98db65c46e3f214ea088
mackaysappor <labs.mackay@gmail.com>
2022-12-30 17:46:32 +0000

    added a welcome controller to welcome users to the app

--- src/main/java/com/stn/postapichallenge/PostApiChallengeApplication.java
+++ src/main/java/com/stn/postapichallenge/PostApiChallengeApplication.java
@@ -2,11 +2,21 @@ package com.stn.postapichallenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class PostApiChallengeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PostApiChallengeApplication.class, args);
+	}
+
+	@RestController
+	class WelcomeController {
+		@GetMapping("/")
+		public String hello() {
+			return "Welcome to the STN Challenge post api";
+		}
 	}
 }
